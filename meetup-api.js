cd7f46ff7d5d4b9199ec4510bbb9e26fb95a7a85
felkug <felicitas.kugland@sinnerschrader.com>
2017-06-02 16:08:23 +0200

    remove unused function

--- meetup-api.js
+++ meetup-api.js
@@ -10,61 +10,6 @@ var resJson;
 
 var url = "https://api.meetup.com/self/calendar?key=1a2c3f15f671076496f104845543012&page=30"
 
-
-function saveData() {
-
-	var dataF = fetch(url)
-		.then(function(res) {
-			resJson = res.json();
-			return res.json();
-		}).then(function(json) {
-			console.log('yes');
-
-			dataBody = resJson.nody;
-
-			for(var key in resJson) {
-
-				var options = { day: 'numeric', month: 'short'};
-
-				var date = new Date(json[key].time);
-				calDate = date.toISOString();
-				
-
-				var dateEnd = new Date(json[key].time);
-				dateEnd.setHours(dateEnd.getHours() + 3);
-				calDateEnd = dateEnd.toISOString();
-
-
-			
-				var venue;
-
-				if(json[key].venue) {
-					venue = json[key].venue['name'] +
-						', ' + json[key].venue['address_1'] +
-						', ' + json[key].venue['city'];
-				}
-				else {
-					venue = 'Needs a location';
-				}
-
-				meetupEvents.meetupEvent.push({ 
-					"dateStart"  : calDate,
-					"dateEnd"  : calDateEnd,
-					"title" : json[key].name,
-					"location": venue,
-					"link":  json[key].link
-				});
-			} 
-			return meetupEvents;
-		});
-
-		console.log('meetup ', dataF.responseJSON);
-		console.log(dataBody);
-
-	
-}
-
-
 // reject für catch - error fall
 module.exports = new Promise(function(resolve, reject){
 	fetch(url)
